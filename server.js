0a938be2dcd0fdf234d9b21e22aefbac29b5d065
hkumariitr <harshitkumarppnews9@gmail.com>
2023-06-26 07:05:43 +0530

    server.s updated

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const dotenv = require("dotenv").config();
 connectDb();
 const app = express();
 const cors = require('cors')
+const path = require("path")
+
+
 app.use(cors());
 
 const port = process.env.PORT || 5000;
@@ -13,6 +16,11 @@ app.use(express.urlencoded({extended:true}));
 app.use("/url",require("./routes/urlRoutes"))
 app.use("/users", require("./routes/userRoutes"));
 
+app.use(express.static(path.join(__dirname, './client/build')))
+
+app.get("*", function(req,res){
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+})
 app.listen(port,()=>{
     console.log(`Server running on port ${port} `);
 });
